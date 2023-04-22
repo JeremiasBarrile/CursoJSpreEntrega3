7074e38218b5b1a1c4b4b3f1ec2688f06dc5c41a
Jere <jere@MBP-de-Jere.fibertel.com.ar>
2023-04-21 22:12:27 -0300

    termino preEntrega3

--- js/main.js
+++ js/main.js
@@ -1,18 +1,21 @@
-
 const container = document.querySelector(".container-card");
 const totalCarrito = document.querySelector("span");
 const filtradorDeAbonos = document.querySelector("input#filterAbonos");
 const carritoAbonos = volverCarro();
 
 function filtrarAbonos(value) {
-  let rta = abonos.filter(abono => abono.nombre.toLocaleLowerCase().includes(value.toLocaleLowerCase()))
-  rta.length > 0 && cargarAbonos(rta)
+  let rta = abonos.filter((abono) =>
+    abono.nombre.toLocaleLowerCase().includes(value.toLocaleLowerCase())
+  );
+  rta.length > 0 && cargarAbonos(rta);
 }
 
-filtradorDeAbonos.addEventListener ("keyup", (e)=> {filtrarAbonos(e.target.value)});
+filtradorDeAbonos.addEventListener("keyup", (e) => {
+  filtrarAbonos(e.target.value);
+});
 
 function returnCardHTML(abono) {
-  let {nombre,importe,codigo} =abono
+  let { nombre, importe, codigo } = abono;
   return `
     <div class="card-abonos">
       <div class="nombre"><p>${nombre}</p></div>
@@ -29,9 +32,11 @@ function cantidadAbonosComprar() {
 cantidadAbonosComprar();
 
 function cargarAbonos(abono) {
-  container.innerHTML = ""
-  abono.forEach(abono => { container.innerHTML += returnCardHTML(abono) })
-  clickButton()
+  container.innerHTML = "";
+  abono.forEach((abono) => {
+    container.innerHTML += returnCardHTML(abono);
+  });
+  clickButton();
 }
 
 function clickButton() {
@@ -39,7 +44,7 @@ function clickButton() {
   if (botones !== null) {
     for (const boton of botones) {
       boton.addEventListener("click", () => {
-        let resp = abonos.find(abono => abono.codigo === parseInt(boton.id));
+        let resp = abonos.find((abono) => abono.codigo === parseInt(boton.id));
         carritoAbonos.push(resp);
         cantidadAbonosComprar();
         carritoLS();
@@ -47,14 +52,12 @@ function clickButton() {
     }
   }
 }
-function carritoLS(){
-  localStorage.setItem("carAbon", JSON.stringify(carritoAbonos))
+function carritoLS() {
+  localStorage.setItem("carAbon", JSON.stringify(carritoAbonos));
 }
-function volverCarro(){
-  return JSON.parse(localStorage.getItem("carAbon")) || []
+function volverCarro() {
+  return JSON.parse(localStorage.getItem("carAbon")) || [];
 }
 
 cargarAbonos(abonos);
 volverCarro();
-
-
